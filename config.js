f522f39fd77e07919a6ad94678fb754fb4b13ab5
pritam_parker <pritamparker@gmail.com>
2017-07-11 19:58:44 +0530

    one more try for heroku deployment

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
-//credentials of postgre
+//credentials of postgres
 //username : postgres
 //password:root
-// connectionString : process.env.DATABASE_URL || 'postgres://avqiapzwhicwyx:0678153f3f20fbb7ac90cfb8aef1426668ec4fe34c736cf14b97cc8e79c6d4a1@ec2-23-23-248-162.compute-1.amazonaws.com:5432/d73mgevoc8jlt8'
+//connectionString : process.env.DATABASE_URL || 'postgres://avqiapzwhicwyx:0678153f3f20fbb7ac90cfb8aef1426668ec4fe34c736cf14b97cc8e79c6d4a1@ec2-23-23-248-162.compute-1.amazonaws.com:5432/d73mgevoc8jlt8'
 connectionString : process.env.DATABASE_URL || 'postgres://postgres:root@localhost:5432/todo';
 
 };
